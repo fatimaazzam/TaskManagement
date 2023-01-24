216dec5ca42918feeb9cc951e0f50c93133ddf11
fatima <fatimaazzam1@gmail.com>
2023-01-24 12:27:07 +0200

    CRUD-OPeration

--- task_app/views.py
+++ task_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # TaskManagement/task_app/views.py
+from django.core.paginator import Paginator
+from django.shortcuts import render
 from django.views.generic import (
                                     ListView,
                                     CreateView,
